1d1b3383e8751a8ff1cf0575135b719839233a65
曾俊豪 <1185902279@qq.com>
2026-04-25 20:20:40 +0800

    refactor(UserController): 优化Spring应用上下文配置
    - 添加FileSystemXmlApplicationContext导入支持
    - 注释掉文件系统路径配置选项供调试使用
    - 保留类路径配置作为默认选项
    - 注释掉直接实例化和类型安全获取bean的代码示例

--- src/main/java/cn/albie/demo/UserController.java
+++ src/main/java/cn/albie/demo/UserController.java
@@ -4,14 +4,18 @@ import cn.albie.service.Impl.UserServiceImpl;
 import cn.albie.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class UserController {
 
     public static void main(String[] args) {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserService userServiceDao=(UserService)app.getBean("userService");
-        userServiceDao.save();
-//        UserService userService=new UserServiceImpl();
-//        userService.save();
+    ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+    //ApplicationContext app = new FileSystemXmlApplicationContext("G:\\projects\\spring260424\\src\\main\\resources\\applicationContext.xml");
+    UserService userServiceDao=(UserService)app.getBean("userService");
+    userServiceDao.save();
+    //UserService userService=new UserServiceImpl();
+    //userService.save();
+    //UserService userService=app.getBean(UserService.class);
+    //userService.save();
     }
 }
